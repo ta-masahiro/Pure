d0647693ab58c9a0a065fe39e002e408257d6334
Masahiro Taniguro <taniguro@yk.rim.or.jp>
2019-10-26 18:56:02 +0900

    ライブラリファイルの追加

---
+++ lib.py
@@ -0,0 +1,93 @@
+{   # 任意個数引数の加算
+    add = lambda(x ..) {
+        n=len(x); 
+        sum = lambda(s,i) 
+            if i>=n: 
+                s
+            : 
+                sum(s+x[i],i+1); 
+        sum(0,0)
+    }
+    ;
+    # mapref([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 1) = [2, 5, 8]
+    def mapref(L,n) = {
+        loop=lambda(i,s) 
+            if i<0 :
+                s 
+            :
+                loop(i-1, [L[i][n]] + s);
+        loop(len(L)-1,[])
+    }
+    ;
+    map = lambda(f,L ..) 
+        if len(L)==1: {
+            loop=lambda(s,i) 
+                if i<0:
+                    s
+                :
+                    loop([f(L[0][i])]+s,i-1);
+            loop([],len(L[0])-1)
+        } : {
+            loop=lambda(s,i) 
+                if i<0 :
+                    s :
+                    loop([apply(f,mapref(L,i))]+s,i-1);
+            loop([],len(L)-1)
+        }
+    ; 
+    range = lambda(l ..) {
+        s=0;
+        e=0;
+        stp=1;
+        n=len(l);
+        if n==1:
+            e=l[0]
+        :
+            if n==2:{
+                e=l[1]
+            }:
+                if n==3:
+                    stp=l[3]
+                :
+                    [];
+
+        loop=lambda(i,r) if i>=e:r:loop(i+stp,r + [i]); 
+        loop(s,[])
+    }
+    ; 
+    # クロージャによる遅延関数の例
+    tarai=lambda(x,y,z) 
+        if x<=y: 
+            y 
+        :{
+            zz=z();
+            tarai(  tarai(x-1,y,lambda() zz),
+                    tarai(y-1,zz,lambda() x),
+                    lambda() tarai(zz-1,x,lambda() y)
+                )
+        }
+    ;
+    count = 0; 
+    t = lambda(x, y, z) { 
+        count = count + 1; 
+        if x <= y:
+	    z
+        :
+            t(  t(x-1, y, z),
+                t(y-1, z, x),
+                t(z-1, x, y)
+            )
+    }
+    ; 
+    memois = lambda(f) {
+        d = dict([1],[1]);
+        lambda(args ..) 
+            if dict_isin(d, args):
+                dict_ref(d, args)
+            :{
+                v = apply(f, args);
+                dict_set(d, args, v);
+                v
+            }
+    }
+}
